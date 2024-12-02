083da223f1d6a3026991110de6457d59b0f67f6c
Fellipe <fellipeaps@kyros.com.br>
2024-12-02 19:34:09 -0300

    Estrutura inicial

--- Meu-projeto/src/Main.java
+++ Meu-projeto/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        float n1, n2;
+        int op;
+
+        System.out.println("----=====Opções====----");
+        System.out.println("| 1 .......... ADIÇÃO |");
+        System.out.println("| 2 ....... SUBTRAÇÃO |");
+        System.out.print(" Opção: ");
+        op = sc.nextInt();
+
+        switch (op) {
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("\n Opção inválida.");
+        }
+
     }
 }
